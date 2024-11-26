93f07e418994cc31de74ae1b8204d7864456308e
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-11-26 12:17:53 +0600

    tailwind css added successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
 import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <h1 className="text-3xl font-bold underline bg-orange-300">
+      Hello world!
+    </h1>
+    </main>
   )
 }
 
