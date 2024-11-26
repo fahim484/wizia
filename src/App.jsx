3a590a13f2081261a24823c617d04d586713ff4c
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-11-26 15:54:38 +0600

    react-router-dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <main>
-      <h1 className="text-3xl font-bold underline bg-orange-300">
-      Hello world!
-    </h1>
-    </main>
-  )
-}
+    <BrowserRouter>
+      <main>
+        <Routes>
+          {/* <Header /> */}
+          <div className="w-full max-w-[1440px] px-2.5 md:px-10 lg:px-20 xl:px-24 mx-auto">
+            <div className="space-y-[128px]">
+              {/* <Route path="" element={<Navbar />}>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/Incidents" element={<Incidents />} />
+                <Route path="/Locations" element={<Locations />} />
+                <Route path="/new-incident" element={<IncidentsFirstPage />} />
+                <Route path="*" element={<Error />} />
+              </Route> */}
+            </div>
+          </div>
+          {/* <Footer /> */}
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
