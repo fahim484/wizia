9f5680ee5b41d3660b7a633e0c0d0cfdad418a9b
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-11-27 10:44:24 +0600

    next ui config setup fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+// 1. import `NextUIProvider` component (Provider Setup)
+import { NextUIProvider } from "@nextui-org/react";
+
+// 2. Wrap NextUIProvider at the root of your app
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
+  </StrictMode>
+);
