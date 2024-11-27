c112dc84d469dd7f405b26921219eab8fe2efc2f
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-11-27 11:33:44 +0600

    google fonts added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
         <Routes>
           {/* <Header /> */}
           <div className="w-full max-w-[1440px] px-2.5 md:px-10 lg:px-20 xl:px-24 mx-auto">
-            <div className="space-y-[128px]">
+            <div className="space-y-[128px] ">
               {/* <Route path="" element={<Navbar />}>
                 <Route path="/" element={<Dashboard />} />
                 <Route path="/Incidents" element={<Incidents />} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,23 @@ export default {
   ],
   theme: {
     container: { center: true },
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00D2FF",
+        secondary: "#2D2D2D",
+        accent: "#666666",
+        foreground: "#ffffff",
+      },
+      // backgroundImage: {
+      //   "gradient-primary": "linear-gradient(180deg, #00D2FF 0%, #059DBE 100%)",
+      //   "gradient-secondary":
+      //     "linear-gradient(161.45deg, #00D2FF -5.54%, #9AEDFF 59.11%)",
+      // },
+      fontFamily: {
+        "heading": ["Montserrat", "sans-serif"],
+        "body": ['"Work Sans"', "sans-serif"],
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
